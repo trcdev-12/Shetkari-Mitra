b686e7a02ef862dc343fb5bc461053e956f59d8f
trcdev-12 <trc.developer02@gmail.com>
2026-09-02 21:33:22 +0530

    Fix deployment setup for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,11 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: '/Shetkari-Mitra',
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
